c262767566c2d7153f22611b801c2b98d0a612f5
dieSehnsucht <3011977177@qq.com>
2025-09-05 17:15:58 +0800

    Remove build directory from repository

--- build/.qt/deploy_PhantomGenesis_529360582e.cmake
+++
@@ -1,8 +0,0 @@
-include("D:/game/qt/PhantomGenesis/build/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/PhantomGenesis-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase;qtmultimedia")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE D:/game/qt/PhantomGenesis/build/PhantomGenesis.exe
-    GENERATE_QT_CONF
-)
--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/.qt/deploy_PhantomGenesis_529360582e.cmake
+++
@@ -1,8 +0,0 @@
-include("D:/game/qt/PhantomGenesis/build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/PhantomGenesis-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase;qtmultimedia")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE D:/game/qt/PhantomGenesis/build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/PhantomGenesis.exe
-    GENERATE_QT_CONF
-)
